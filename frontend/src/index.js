422c4644393754f84b2fa2cd00e2332a0b688fe6
julian guardiola <161070480+xrokirambox@users.noreply.github.com>
2026-04-23 12:40:21 -0500

    fix: remove comma in ReactDOM render

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";git add .
-git commit -m "fix: remove alias @ from App import"
-git push
+import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
