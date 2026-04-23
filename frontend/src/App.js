976042011e9b7674c0542591f1672d8954f250cd
julian guardiola <161070480+xrokirambox@users.noreply.github.com>
2026-04-23 12:52:10 -0500

    fix: remove trailing comma in ReactDOM render

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "@/App.css";
+import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "sonner";
 import { LibraryProvider, useLibrary } from "./context/LibraryContext";
