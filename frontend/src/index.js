841a6cb5d69d26b1273cfe37b506041924b4e3d6
julian guardiola <161070480+xrokirambox@users.noreply.github.com>
2026-04-23 12:32:47 -0500

    fix: remove alias @ from App import

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,7 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "@/App";
+import App from "./App";git add .
+git commit -m "fix: remove alias @ from App import"
+git push
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
