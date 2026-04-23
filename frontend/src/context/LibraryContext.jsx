ed7484f7be69eb301f12e691355fe8699f84d15b
julian guardiola <161070480+xrokirambox@users.noreply.github.com>
2026-04-23 14:06:17 -0500

    fix: use correct collection name links

--- frontend/src/context/LibraryContext.jsx
+++ frontend/src/context/LibraryContext.jsx
@@ -1,7 +1,6 @@
 import React, { createContext, useContext, useEffect, useState, useCallback } from "react";
 import axios from "axios";
 
-//const API = `${process.env.REACT_APP_API_URL}/api`;
 const API = `${process.env.REACT_APP_API_URL || "http://localhost:8000"}/api`;
 const LibraryContext = createContext(null);
 
